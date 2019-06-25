d5edb5e4177a35a2c104afa53081c27a5e0d5281
Vitoria <vitoriagoncalvesgaspar@hotmail.com>
2019-06-24 23:01:26 -0300

    Finalization of animation

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+import { Animated } from 'react-native';
+import { PanGestureHandler, State } from 'react-native-gesture-handler';
 import {
   Container,
   Content,
@@ -16,33 +18,72 @@ import Tabs from '../../components/tabs';
 import Menu from '../../components/menu';
 
 export default function Main() {
+  const offset = 0;
+  const translateY = new Animated.Value(0);
+
+  const animatedEvent = Animated.event(
+    [
+      {
+        nativeEvent: {
+          translationY: translateY,
+        },
+      },
+    ],
+    { useNativeDriver: true },
+  );
+
+  function onHandlerStateChange(event) {
+    if (event.nativeEvent.oldState === State.ACTIVE) {
+      const { translationY } = event.nativeEvent;
+
+      offset + -translationY;
+
+      translateY.setOffset(offset);
+      translateY.setValue(0);
+    }
+  }
+
   return (
     <Container>
       <Header />
 
       <Content>
-        <Menu />
+        <Menu translateY={translateY} />
 
-        <Card>
-          <CardHeader>
-            <Icon name="attach-money" size={28} color="#666" />
-            <Icon name="visibility-off" size={28} color="#666" />
-          </CardHeader>
+        <PanGestureHandler
+          onGestureEvent={animatedEvent}
+          onHandlerStateChange={onHandlerStateChange}
+        >
+          <Card style={{
+            transform: [{
+              translateY: translateY.interpolate({
+                inputRange: [-350, 0, 380],
+                outputRange: [-50, 0, 380],
+                extrapolate: 'clamp',
+              }),
+            }],
+          }}
+          >
+            <CardHeader>
+              <Icon name="attach-money" size={28} color="#666" />
+              <Icon name="visibility-off" size={28} color="#666" />
+            </CardHeader>
 
-          <CardContent>
-            <Title>Saldo disponível</Title>
-            <Description>R$ 199.999,99</Description>
-          </CardContent>
+            <CardContent>
+              <Title>Saldo disponível</Title>
+              <Description>R$ 199.999,99</Description>
+            </CardContent>
 
-          <CardFooter>
-            <Annotation>
-                Transferência de R$ 20,00 recebida de Vitoria Gonçalves hoje às 16:00h
-            </Annotation>
-          </CardFooter>
-        </Card>
+            <CardFooter>
+              <Annotation>
+                  Transferência de R$ 20,00 recebida de Vitoria Gonçalves hoje às 16:00h
+              </Annotation>
+            </CardFooter>
+          </Card>
+        </PanGestureHandler>
       </Content>
 
-      <Tabs />
+      <Tabs translateY={translateY} />
     </Container>
   );
 }
