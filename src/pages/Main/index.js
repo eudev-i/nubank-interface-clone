1833b6ec0076bf0e528f3dc4b8c89e1cf7080ddf
Vitoria <vitoriagoncalvesgaspar@hotmail.com>
2019-06-24 21:22:21 -0300

    Finalization of card

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,12 +1,45 @@
 import React from 'react';
-import { Container } from './styles';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import {
+  Container,
+  Content,
+  Card,
+  CardHeader,
+  CardContent,
+  Title,
+  Description,
+  CardFooter,
+  Annotation,
+} from './styles';
 import Header from '../../components/header';
 import Tabs from '../../components/tabs';
+
 
 export default function Main() {
   return (
     <Container>
       <Header />
+
+      <Content>
+        <Card>
+          <CardHeader>
+            <Icon name="attach-money" size={28} color="#666" />
+            <Icon name="visibility-off" size={28} color="#666" />
+          </CardHeader>
+
+          <CardContent>
+            <Title>Saldo disponível</Title>
+            <Description>R$ 199.999,99</Description>
+          </CardContent>
+
+          <CardFooter>
+            <Annotation>
+                Transferência de R$ 20,00 recebida de Vitoria Gonçalves hoje às 16:00h
+            </Annotation>
+          </CardFooter>
+        </Card>
+      </Content>
+
       <Tabs />
     </Container>
   );
